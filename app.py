99a0638948dc1158a1623aabf6095b261fd8d6e4
Hong-Jin-seo <jinseoh123@gmail.com>
2026-01-29 12:54:51 +0900

    Feat: 텍스트 앞 번호 삭제

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ if st.session_state.todos:
     for i, todo in enumerate(st.session_state.todos):
         # st.write(f'{i + 1}번째 todo : {todo}')
         col1, col2 = st.columns([0.1, 0.9])
-        col1.checkbox(f'{i + 1}', value=todo.get_done(), key=f'done_{i}', on_change=togle_done, args=(i,))
+        # col1.checkbox(f'{i + 1}', value=todo.get_done(), key=f'done_{i}', on_change=togle_done, args=(i,))
+        col1.checkbox(value=todo.get_done(), key=f'done_{i}', on_change=togle_done, args=(i,))
         col2.markdown(f'~~{todo.get_task()}~~' if todo.get_done() else todo.get_task())
 else:
     st.info('할 일을 추가해보세요.')
